945c378d8bbca3222a0ab3f0926ae974d5198d69
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-24 18:23:46 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRANDS = ["Uggs", "Rainbow", "Nike", "Nike"]
+  BRANDS = ["Uggs", "Rainbow"]
 
   def initialize(brand)
     @brand = brand
@@ -13,8 +13,6 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand
-      BRANDS.size + 7
-    end
+  
 
 end
